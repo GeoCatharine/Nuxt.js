bbd2f3d6d2ae4ecd1d28842b3bbf996ae088f43c
GeometriCat <76999282+GeoCatharine@users.noreply.github.com>
2021-09-09 10:51:21 +0200

    Add files via upload

---
+++ nuxt/layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <PageNotFound />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .container {
+        margin-top: 5rem;
+        padding: 5rem 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 70vh;
+    }
+</style>
